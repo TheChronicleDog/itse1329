e1a04ea4eeae7e05e62284cd15ecef09c999c24c
Chronicle_Dog <minicast2302@gmail.com>
2022-09-17 17:32:07 -0500

    Posting Bank Charges

---
+++ src/main/kotlin/week03/03.8_Bank_Charges.kt
@@ -0,0 +1,26 @@
+import java.util.*
+//Hill, 03.8, 9/17/2022
+fun main(){
+    val checks = recieveInfo("checks")
+    calculateChecksAndPrint(checks)
+
+
+}
+fun recieveInfo(questionType: String): Double {
+    val scanner = Scanner(System.`in`)
+    print("How many $questionType have you written this month? >> ")
+    val value = scanner.nextDouble()
+    scanner.nextLine()
+    return value
+}
+fun calculateChecksAndPrint(checkNum : Double){
+    var totalVal :Double = 10.0;
+    when {
+        checkNum < 20 -> totalVal = 10 + (checkNum* .10)
+        checkNum in 20.0..39.0 -> totalVal = 10 + (checkNum* .08)
+        checkNum in 40.0..59.0 -> totalVal = 10 + (checkNum* .06)
+        checkNum >= 60 -> totalVal = 10 + (checkNum* .04)
+    }
+    print("Bank Service fee for $checkNum checks in month: $totalVal")
+
+}
