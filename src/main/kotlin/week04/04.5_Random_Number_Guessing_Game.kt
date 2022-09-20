d02c7e7ee9fa753b87f6a9be74893d8072372186
Chronicle_Dog <minicast2302@gmail.com>
2022-09-20 10:46:37 -0500

    Publishing 04.5 random number game

---
+++ src/main/kotlin/week04/04.5_Random_Number_Guessing_Game.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+//The Doctor, 04.5, 9/20/22
+fun main() {
+    val input = Scanner(System.`in`)
+    val random = (0..100).random()
+    print("Please guess a value between 0 and 100 including both >> ")
+    val inputVal = input.nextInt()
+    var newVal = inputVal
+
+    while (newVal != random) {
+        if (newVal > random) {
+            println("Too high, try again")
+        } 
+        else if (newVal < random) {
+            println("Too low, try again")
+        }
+        print("Please guess a value between 0 and 100 including both >> ")
+        newVal = input.nextInt()
+    }
+    print("Nice you got it! The value is $random!")
+
+}
