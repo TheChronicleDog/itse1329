1baf0d5fac198e50a51a07974ac4ec926771ab5a
Chronicle_Dog <minicast2302@gmail.com>
2022-09-20 10:09:30 -0500

    Publishing 04.3 Population

---
+++ src/main/kotlin/week04/04.3_Population.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+fun main() {
+    var startPop = inputInfo("What is the starting number of organisms? >> ")
+    startPop = misInputCheck(startPop,2.0,"What is the starting number of organisms? >> ")
+    var popInc = inputInfo("Daily population increase? >> ")
+    popInc = misInputCheck(popInc,0.0,"Daily population increase? >> ")
+    var days = inputInfo("For how many days? >> ")
+    days = misInputCheck(days,1.0,"For how many days? >> ")
+    var newPop =startPop
+
+    println("Day 0, Population:${newPop.toInt()}")
+    for (i in 1..days.toInt()) {
+        newPop += popInc * newPop
+        println("Day $i, Population:${newPop.toInt()}")
+    }
+
+}
+
+fun inputInfo(question: String): Double {
+    val response = Scanner(System.`in`)
+    print(question)
+    return response.nextDouble()
+}
+fun misInputCheck(value: Double, bottomRange: Double, restateQuestion:String):Double{
+    val response = Scanner(System.`in`)
+    var newValue= value
+    while((newValue < bottomRange)) {//error message
+        println("ERROR Please enter a valid value")
+        println("")
+        println(restateQuestion)
+        newValue =  response.nextDouble()
+    }
+    return newValue
+
+}
